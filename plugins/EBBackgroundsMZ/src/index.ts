70b0d55d1172521434c205e2dd82ffd9165ed38c
Jengamon <uche.okwo1@gmail.com>
2021-11-27 11:59:00 -0500

    Figure out how overloading works!

--- plugins/EBBackgroundsMZ/src/index.ts
+++ plugins/EBBackgroundsMZ/src/index.ts
@@ -1,7 +1,8 @@
 import * as json5 from "json5";
-import { $gameSystem, PluginManager, Spriteset_Battle } from "rmmz";
-import "./layer_config";
+import { $gameSystem, PluginManager, Scene_Battle, Sprite, Spriteset_Battle, Sprite_Battleback } from "rmmz";
+import { Configuration, EBBFilter } from "./layer_config";
 import { getFileFromServer } from "./utils";
+import * as PIXI from "pixi.js";
 
 const parameters = PluginManager.parameters("JM_EarthBoundBackgroundsMZ");
 
@@ -23,7 +24,49 @@ export function greet(name: string) {
     return `Hello, ${name}`;
 }
 
-// const SB_createDisplayObjects = Spriteset_Battle.prototype.createBattleback;
-// Spriteset_Battle.prototype.createBattleback = function() {
+const configuration1 = new Configuration({
+    type: "horizontal",
+    amplitude: 10,
+    frequency: 5,
+});
 
-// };
+const configuration2 = new Configuration({
+    type: "horizontal_interlaced",
+    amplitude: 30,
+    frequency: 45,
+});
+
+declare global {
+    interface Spriteset_Battle {
+        _back1Sprite: Sprite_Battleback;
+        _back2Sprite: Sprite_Battleback;
+        createBackground(): void;
+        createBattleback(): void;
+    }
+}
+
+const SB_createBackground = Spriteset_Battle.prototype.createBackground;
+Spriteset_Battle.prototype.createBackground = function (this: Spriteset_Battle) {
+    SB_createBackground.apply(this, []);
+
+    const _ebb1Sprite = new Sprite();
+    const _ebb2Sprite = new Sprite();
+
+    _ebb1Sprite.filters = [new EBBFilter(configuration1)];
+    _ebb2Sprite.filters = [new EBBFilter(configuration2)];
+
+    this._backgroundSprite.addChild(_ebb1Sprite);
+    this._backgroundSprite.addChild(_ebb2Sprite);
+
+    console.log("ghas");
+};
+
+const SB_createBattleback = Spriteset_Battle.prototype.createBattleback;
+Spriteset_Battle.prototype.createBattleback = function (this: Spriteset_Battle) {
+    SB_createBattleback.apply(this, []);
+
+    this._back1Sprite.visible = false;
+    this._back2Sprite.visible = false;
+
+    console.log("goomba");
+}
